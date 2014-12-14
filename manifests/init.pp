03ebd0839d7ff8d1d5a356f64b9d657bef2d74c5
Heesang Chae <theand@gmail.com>
2014-12-14 14:57:31 +0900

    update mysql workbench version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class mysql_workbench {
   package { 'mysql_workbench':
     provider => 'appdmg',
-    source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-gpl-6.0.2-osx-i686.dmg/from/http://cdn.mysql.com/'
+    source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-osx-i686.dmg/from/http://cdn.mysql.com/'
   }
 }
