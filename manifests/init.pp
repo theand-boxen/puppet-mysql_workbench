4ea9382153ad7b6b96c7628554b6e0770d4aa900
Heesang Chae <theand@daumcorp.com>
2015-07-26 17:22:32 +0900

    update to v6.3.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class mysql_workbench {
   package { 'mysql_workbench':
     provider => 'appdmg',
-    source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-osx-i686.dmg/from/http://cdn.mysql.com/'
+    source   => 'https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.4-osx-x86_64.dmg'
   }
 }
--- spec/classes/mysql_workbench_spec.rb
+++ spec/classes/mysql_workbench_spec.rb
@@ -5,7 +5,7 @@ describe 'mysql_workbench' do
     should contain_class('mysql_workbench')
     should contain_package('mysql_workbench').with({
       :provider => 'appdmg',
-      :source   => 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-osx-i686.dmg/from/http://cdn.mysql.com/'
+      :source   => 'https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.4-osx-x86_64.dmg'
     })
   end
 end
